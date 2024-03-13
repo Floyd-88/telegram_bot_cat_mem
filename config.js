0d6844219da7c87ff386b002412986486f89a3a6
Dio Eracler <dioeracler1988@gmail.com>
2024-03-13 14:45:41 +0300

    .env

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
+import 'dotenv/config'
 export const config = {
-    telegramToken: "7088922660:AAEuxDmeY5JRfi26F8ojd_Zp0XN7IAzjzrE",
+    telegramToken: process.env.BOT_API_KEY,
     catUrl: "https://api.thecatapi.com/v1/images/search",
   };
